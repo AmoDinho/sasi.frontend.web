8b5f8c17046712f8d68aa69a805bf7e6aba5998f
AmoDinho <u14284783@tuks.co.za>
2021-02-02 22:25:56 +0200

    commented stuff out

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import App from "../components/App";
-import { initializeApollo, addApolloState } from "../lib/apolloClient";
+// import { initializeApollo, addApolloState } from "../lib/apolloClient";
 
 const IndexPage = () => (
   <App>
@@ -7,18 +7,18 @@ const IndexPage = () => (
   </App>
 );
 
-export async function getStaticProps() {
-  const apolloClient = initializeApollo();
+// export async function getStaticProps() {
+//   const apolloClient = initializeApollo();
 
-  await apolloClient.query({
-    query: ALL_POSTS_QUERY,
-    variables: allPostsQueryVars,
-  });
+//   await apolloClient.query({
+//     query: ALL_POSTS_QUERY,
+//     variables: allPostsQueryVars,
+//   });
 
-  return addApolloState(apolloClient, {
-    props: {},
-    revalidate: 1,
-  });
-}
+//   return addApolloState(apolloClient, {
+//     props: {},
+//     revalidate: 1,
+//   });
+// }
 
 export default IndexPage;
