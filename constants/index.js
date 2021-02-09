28ff376e4fffa58a8b25e7517ec9d8a16d6dd276
AmoDinho <u14284783@tuks.co.za>
2021-02-09 21:50:51 +0200

    added mixpanel function

--- constants/index.js
+++ constants/index.js
@@ -1,15 +1,19 @@
+import mixpanel from "mixpanel-browser";
+mixpanel.init(process.env.GATSBY_MIXPANEL);
+export const track = (name, props) => {
+  mixpanel.track(name, props);
+};
 export const socialLinks = [
-    {
-        url: "https://www.pinterest.co.uk/real_sasi/_saved/",
-        icon: "PT",
-    },
-    {
-        url: "https://twitter.com/sasi_world ",
-        icon: "TW",
-    },
-    {
-        url: "https://www.instagram.com/sasi.world/",
-        icon: "IG",
-    },
-
-]
+  {
+    url: "https://www.pinterest.co.uk/real_sasi/_saved/",
+    icon: "PT",
+  },
+  {
+    url: "https://twitter.com/sasi_world ",
+    icon: "TW",
+  },
+  {
+    url: "https://www.instagram.com/sasi.world/",
+    icon: "IG",
+  },
+];
