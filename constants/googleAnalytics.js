6441006202b5e973c2a1f9d840bdceb110b2224b
AmoDinho <u14284783@tuks.co.za>
2021-02-09 22:18:04 +0200

    added the stuff

--- constants/googleAnalytics.js
+++ constants/googleAnalytics.js
@@ -1,3 +1,15 @@
 import ReactGA from "react-ga";
 
 export const initGA = () => ReactGA.initialize("G-KHMV0D6Z5S");
+
+export const logPageView = ()=>{
+    ReactGA.set({page: window.location.pathname})
+    ReactGA.pageview(window.location.pathname)
+
+}
+
+export const logEvent = (category="",action=""){
+    if(category && action){
+        ReactGA.event({category,action})
+    }
+}
