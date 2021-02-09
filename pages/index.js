5456bdd85a36aca55e8268c0db3c500d368358d6
AmoDinho <u14284783@tuks.co.za>
2021-02-09 21:52:35 +0200

    added tracking

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,12 @@ import App from "../components/App";
 import { SocialButton } from "../components/ui/buttons";
 import { BodyOne } from "../components/ui/Typography";
 import { HeadingOne } from "../components/ui/Typography";
-import { HeadingTwo } from "../components/ui/Typography";
-import { socialLinks } from "../constants";
+import { socialLinks, track } from "../constants";
 
 // import { initializeApollo, addApolloState } from "../lib/apolloClient";
 
 const IndexPage = () => {
+  track("App.HomePage");
   return (
     <App>
       <HeadingOne className="text-center flex justify-center my-2 ">
