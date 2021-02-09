3dd1833a22a6e7494d6804a7a814defa88a65693
AmoDinho <u14284783@tuks.co.za>
2021-02-09 21:45:20 +0200

    added mailchimp

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,7 @@ export default function App({ Component, pageProps }) {
           }}
         />
         <script
+          id="mcjs"
           dangerouslySetInnerHTML={{
             __html: `function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/4abc598255676670b7524b68c/4af7df9e645a7b613206abef1.js");`,
           }}
