1bcaf17750697131010e7f44dff28194a9f26b4d
AmoDinho <u14284783@tuks.co.za>
2021-02-09 22:26:25 +0200

    fixed issue

--- constants/googleAnalytics.js
+++ constants/googleAnalytics.js
@@ -2,14 +2,13 @@ import ReactGA from "react-ga";
 
 export const initGA = () => ReactGA.initialize("G-KHMV0D6Z5S");
 
-export const logPageView = ()=>{
-    ReactGA.set({page: window.location.pathname})
-    ReactGA.pageview(window.location.pathname)
+export const logPageView = () => {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+};
 
-}
-
-export const logEvent = (category="",action=""){
-    if(category && action){
-        ReactGA.event({category,action})
-    }
-}
+export const logEvent = (category = "", action = "") => {
+  if (category && action) {
+    ReactGA.event({ category, action });
+  }
+};
