0f83bac445906ac3b407e0b5dcc20f664ea6a5b5
AmoDinho <u14284783@tuks.co.za>
2021-02-16 23:23:23 +0200

    added show hooks

--- components/ui/cards/PhotoCard.js
+++ components/ui/cards/PhotoCard.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Image from "next/image";
 
 const PhotoCard = ({ imageURL, imageAlt, className }) => {
+  const [show, setShow] = useState(false);
   return (
-    <img
-      src={imageURL}
-      alt={imageAlt}
-      className={`${className} rounded-lg `}
-      //   width={250}
-      //   height={100}
-    />
+    <div>
+      <img
+        src={imageURL}
+        alt={imageAlt}
+        className={`${className} rounded-lg `}
+        onMouseOver={() => setShow(!show)}
+        onMouseLeave={() => setShow(!show)}
+        //   width={250}
+        //   height={100}
+      />
+    </div>
   );
 };
 
