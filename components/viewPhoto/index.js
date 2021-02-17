0da21ca41a950506f6963e779e89c40796910a03
Brandon <brandon.dlamini95@gmail.com>
2021-02-17 23:19:45 +0200

    added view photo query to index.js

--- components/viewPhoto/index.js
+++ components/viewPhoto/index.js
@@ -1,7 +1,22 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import PhotoBlock from "./PhotoBlock";
+import { useQuery } from "@apollo/client";
+import { GET_A_PHOTO } from "../../graphql/photos/queries";
 const ViewPhoto = () => {
-  return <>hi</>;
+  const { data, loading, error } = useQuery(GET_A_PHOTO, {
+    variables: {
+      ID: "",
+    },
+  });
+
+  if (data) {
+    console.log(data);
+  }
+  return (
+    <>
+      <PhotoBlock />
+    </>
+  );
 };
 
 export default ViewPhoto;
