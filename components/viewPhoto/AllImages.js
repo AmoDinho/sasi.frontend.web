031cea8ef619f2e12237fe2dc6643eabe9448d9f
AmoDinho <u14284783@tuks.co.za>
2021-02-18 23:38:42 +0200

    imported the stuff

--- components/viewPhoto/AllImages.js
+++ components/viewPhoto/AllImages.js
@@ -4,7 +4,10 @@ import { HeadingTwo } from "../Typography";
 import HeadingTwo from "../Typography/HeadingTwo";
 import { GET_ALL_PHOTOS } from "../../graphql/photos/queries";
 
-const AllImages = () => {
+const AllImages = ({ id }) => {
+  const [allPhotos, setAllPhotos] = useState([]);
+  const { data, loading, error } = useQuery(GET_ALL_PHOTOS);
+
   return (
     <div>
       <HeadingTwo>Hey we've got some nice things!</HeadingTwo>
