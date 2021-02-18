33f9301f84130ad5069c0b78fc9de869ab0c3b7c
AmoDinho <u14284783@tuks.co.za>
2021-02-18 23:43:54 +0200

    imported allimages

--- components/viewPhoto/index.js
+++ components/viewPhoto/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import PhotoBlock from "./PhotoBlock";
 import { useQuery } from "@apollo/client";
 import { GET_A_PHOTO } from "../../graphql/photos/queries";
+import AllImages from "./AllImages";
 const ViewPhoto = (props) => {
   const { id } = props;
   const [photo, setPhoto] = useState({});
@@ -22,9 +23,10 @@ const ViewPhoto = (props) => {
   if (loading) return <p>Loading</p>;
   if (error) return <p>ALL out of SAS.</p>;
   return (
-    <>
+    <div className="grid grid-rows-2">
       <PhotoBlock photo={photo} />
-    </>
+      <AllImages id={id} />
+    </div>
   );
 };
 
