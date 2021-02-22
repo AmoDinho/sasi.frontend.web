211d34ad2e00dd4b4fbf730a2c92d3931ed3debc
AmoDinho <u14284783@tuks.co.za>
2021-02-22 22:39:02 +0200

    passed down the isOPEN thing

--- components/viewPhoto/PurchaseModal.js
+++ components/viewPhoto/PurchaseModal.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import PropTypes from "prop-types";
+import Modal from "../ui/modals/Modal";
+import { HeadingOne } from "../ui/Typography";
 const PurchaseModal = (props) => {
-  return <></>;
+  return (
+    <>
+      <Modal isOpen={props.isOpen}>
+        <div>
+          <HeadingOne>Please leave your email before you download</HeadingOne>
+        </div>
+      </Modal>
+    </>
+  );
 };
+
+PurchaseModal.propTypes = {};
 
 export default PurchaseModal;
