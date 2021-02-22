38e4722b94986efbd170ae11b853d6e188800d68
AmoDinho <u14284783@tuks.co.za>
2021-02-22 22:32:23 +0200

    done with basic modal

--- components/ui/modals/PurchaseModal.js
+++ components/ui/modals/PurchaseModal.js
@@ -1,7 +1,22 @@
-import React from "react";
-
-const PurchaseModal = (props) => {
-  return <></>;
+import React, { Children } from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+const PurchaseModal = ({ props, children }) => {
+  return (
+    <>
+      <Modal isOpen={props.isOpen} onClose={props.onClose}>
+        <ModalOverlay />
+        <ModalContent>{children}</ModalContent>
+      </Modal>
+    </>
+  );
 };
 
 export default PurchaseModal;
