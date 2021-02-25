8c6d0bf7a1058c234562aa61c90336bd80710603
AmoDinho <u14284783@tuks.co.za>
2021-02-25 21:41:53 +0200

    added text

--- components/viewPhoto/AttributionModal.js
+++ components/viewPhoto/AttributionModal.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Modal from "../ui/modals/Modal";
 
-const AttributionModal = ({}) => {
-  return <div></div>;
+const AttributionModal = (props) => {
+  return (
+    <Modal isOpen={props.isOpen} onClose={props.onClose}>
+      <div>This is how to attribute</div>
+    </Modal>
+  );
 };
 
 export default AttributionModal;
