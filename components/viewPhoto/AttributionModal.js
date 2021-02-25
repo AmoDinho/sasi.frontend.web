14d526b7db3dfb20bdf3ec2067663cde12c745ec
AmoDinho <u14284783@tuks.co.za>
2021-02-25 22:00:29 +0200

    imported heading one

--- components/viewPhoto/AttributionModal.js
+++ components/viewPhoto/AttributionModal.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Modal from "../ui/modals/Modal";
-
+import { SocialButton } from "../ui/buttons";
+import { socialIcons } from "../../constants";
+import { HeadingOne } from "../ui/Typography";
 const AttributionModal = (props) => {
   return (
-    <Modal isOpen={props.isOpen} onClose={props.onClose}>
-      <div>This is how to attribute</div>
+    <Modal isOpen={props.isOpen} onClose={() => props.onClose()}>
+      <div className="p-5">
+        <HeadingOne>This is how to attribute</HeadingOne>
+      </div>
     </Modal>
   );
 };
