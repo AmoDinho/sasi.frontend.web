d364c6202ae8f7482eb2ddd09b5e6c4d2682bac5
AmoDinho <u14284783@tuks.co.za>
2021-02-25 22:23:31 +0200

    added the copy command

--- components/viewPhoto/AttributionModal.js
+++ components/viewPhoto/AttributionModal.js
@@ -1,25 +1,36 @@
-import React ,{useState}from "react";
+import React, { useState, useRef } from "react";
 import { CopyIcon } from "@chakra-ui/icons";
-import { IconButton,useClipboard } from "@chakra-ui/react";
+import { IconButton } from "@chakra-ui/react";
 import Modal from "../ui/modals/Modal";
 import { SocialButton } from "../ui/buttons";
 import { socialIcons } from "../../constants";
 import { HeadingOne } from "../ui/Typography";
 const AttributionModal = (props) => {
   const { socials } = props;
-  const { hasCopied, onCopy } = useClipboard()
-const [copyValue,setCopyValue] =useState("")
+  const [hasCopied, setHasCopied] = useState(false);
+  const linkRef = useRef(null);
+
+  const copyToClipboard = (e) => {
+    linkRef.current.select();
+    document.execCommand("copy");
+    setHasCopied(true);
+  };
+  console.log("linkRef", linkRef);
   return (
     <Modal isOpen={props.isOpen} onClose={() => props.onClose()}>
       <div className="p-5">
         <HeadingOne>This is how to attribute</HeadingOne>
         <div>
           {socials.map((social, idx) => (
-            <SocialButton key={idx} url={social.url}>
-              {socialIcons[social.name]}
-            </SocialButton>
-                      <IconButton icon={<CopyIcon />} />
-
+            <div>
+              <SocialButton key={idx} url={social.url}>
+                {socialIcons[social.name]}
+              </SocialButton>
+              <p>{social.name}</p>
+              <IconButton
+                icon={<CopyIcon onClick={() => copyToClipboard()} />}
+              />
+            </div>
           ))}
         </div>
       </div>
