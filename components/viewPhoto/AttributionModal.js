45f92022363974edcdf2fa437b6c5856fe965838
AmoDinho <u14284783@tuks.co.za>
2021-02-25 22:10:27 +0200

    added use state

--- components/viewPhoto/AttributionModal.js
+++ components/viewPhoto/AttributionModal.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React ,{useState}from "react";
 import { CopyIcon } from "@chakra-ui/icons";
-import { IconButton } from "@chakra-ui/react";
+import { IconButton,useClipboard } from "@chakra-ui/react";
 import Modal from "../ui/modals/Modal";
 import { SocialButton } from "../ui/buttons";
 import { socialIcons } from "../../constants";
 import { HeadingOne } from "../ui/Typography";
 const AttributionModal = (props) => {
   const { socials } = props;
+  const { hasCopied, onCopy } = useClipboard()
+const [copyValue,setCopyValue] =useState("")
   return (
     <Modal isOpen={props.isOpen} onClose={() => props.onClose()}>
       <div className="p-5">
@@ -16,8 +18,9 @@ const AttributionModal = (props) => {
             <SocialButton key={idx} url={social.url}>
               {socialIcons[social.name]}
             </SocialButton>
+                      <IconButton icon={<CopyIcon />} />
+
           ))}
-          <IconButton icon={<CopyIcon />} />
         </div>
       </div>
     </Modal>
