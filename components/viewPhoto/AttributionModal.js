2f877b8e4c8e5dbd4c379d8adb6460458164867a
AmoDinho <u14284783@tuks.co.za>
2021-02-25 22:02:01 +0200

    mapping over socails

--- components/viewPhoto/AttributionModal.js
+++ components/viewPhoto/AttributionModal.js
@@ -4,10 +4,18 @@ import { SocialButton } from "../ui/buttons";
 import { socialIcons } from "../../constants";
 import { HeadingOne } from "../ui/Typography";
 const AttributionModal = (props) => {
+  const { socials } = props;
   return (
     <Modal isOpen={props.isOpen} onClose={() => props.onClose()}>
       <div className="p-5">
         <HeadingOne>This is how to attribute</HeadingOne>
+        <div>
+          {socials.map((social, idx) => (
+            <SocialButton key={idx} url={social.url}>
+              {socialIcons[social.name]}
+            </SocialButton>
+          ))}
+        </div>
       </div>
     </Modal>
   );
